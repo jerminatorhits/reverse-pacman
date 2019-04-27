2d2516095ec3d3c05c5b11f3ca369b44b69c9569
Jeremy Kwa <jeremykwa@gmail.com>
2019-04-26 20:13:44 -0700

    More walls

--- game.js
+++ game.js
@@ -3,14 +3,32 @@ console.log('hello from game.js');
 const walls = [
   [ 16, 24, 224, 2 ],
   [ 16, 24, 2, 56 ],
-  [ 16, 80, 64, 2 ],
-  [ 80, 80, 2, 32 ],
-  [ 16, 112, 66, 2 ],
-  [ 16, 134, 66, 2 ],
-  [ 80, 134, 2, 32 ],
-  [ 16, 166, 66, 2 ],
+  [ 16, 80, 44, 2 ],
+  [ 58, 80, 2, 32 ],
+  [ 16, 112, 44, 2 ],
+  [ 16, 134, 44, 2 ],
+  [ 58, 134, 2, 32 ],
+  [ 16, 166, 44, 2 ],
   [ 16, 166, 2, 60 ],
   [ 16, 224, 224, 2 ],
+
+  // edge extrusions
+  [ 125, 24, 6, 28 ],
+  [ 16, 193, 18, 6 ],
+
+  // inner blocks
+  [ 32, 38, 28, 10 ],
+  [ 74, 38, 36, 10 ],
+
+  // Upper T
+  [ 92, 62, 36, 4 ],
+  [ 125, 62, 6, 28 ],
+
+  [ 32, 62, 27, 4 ],
+  [ 74, 62, 4, 52 ],
+  [ 74, 88, 26, 4 ],
+
+
 
 ];
 
@@ -25,9 +43,9 @@ context.fillStyle = "#000";
 context.fillRect(0, 0, 256, 256);
 
 function drawBoard(context) {
-  context.fillStyle = "#00F";
+  context.strokeStyle = "#00F";
   walls.forEach(wall => {
-    context.fillRect(wall[0], wall[1], wall[2], wall[3]);
+    context.strokeRect(wall[0], wall[1], wall[2], wall[3]);
   });
 }
 drawBoard(context);
