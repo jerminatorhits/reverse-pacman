6272eb5a61376053699ca1efc7d45312dc5f93c5
Jeremy Kwa <jeremykwa@gmail.com>
2019-04-26 19:22:28 -0700

    Some walls

--- game.js
+++ game.js
@@ -1,21 +1,24 @@
 console.log('hello from game.js');
 
 const walls = [
-  [ 16, 15, 10, 10 ]
+  [ 16, 16, 224, 2 ],
+  [ 16, 16, 2, 48 ],
+  [ 16, 64, 64, 2 ],
+  [ 80, 64, 2, 32 ],
+
+  [ 16, 224, 224, 2 ],
+
 ];
 
 const canvas = document.getElementById('game');
 const context = canvas.getContext('2d');
 
-context.fillColor = "#000";
+context.fillStyle = "#000";
 context.fillRect(0, 0, 256, 256);
 
-
 function drawBoard(context) {
-  context.fillColor = "#0000FF";
-
+  context.fillStyle = "#00F";
   walls.forEach(wall => {
-    console.log(wall);
     context.fillRect(wall[0], wall[1], wall[2], wall[3]);
   });
 }
